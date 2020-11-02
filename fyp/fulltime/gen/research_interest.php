00e04836a659554dd56ae1fddee0a5c98a451957
teckzong <teckzong_1996@hotmail.com>
2020-11-02 23:26:02 +0800

    Research Interest search

--- fyp/fulltime/gen/research_interest.php
+++ fyp/fulltime/gen/research_interest.php
@@ -89,10 +89,16 @@ if($pageNum_researchInterest == ''){
 
 $startRow_researchInterest = $pageNum_researchInterest * $maxRow_researchInterest;
 
+// $query_rsResearchInterest = "SELECT name, staff_id, GROUP_CONCAT(interest SEPARATOR '---') AS interests FROM " . $TABLES['research_interest'] .
+//     " LEFT JOIN " . $TABLES['staff'] .
+//     " ON research_interest.staff_id = staff.id " .
+//     " WHERE staff_id LIKE ? AND (staff_id LIKE ? OR name LIKE ? OR interest LIKE ?) GROUP BY name, staff_id ORDER BY name ASC ";
+
 $query_rsResearchInterest = "SELECT name, staff_id, GROUP_CONCAT(interest SEPARATOR '---') AS interests FROM " . $TABLES['research_interest'] .
     " LEFT JOIN " . $TABLES['staff'] .
     " ON research_interest.staff_id = staff.id " .
-    " WHERE staff_id LIKE ? AND (staff_id LIKE ? OR name LIKE ? OR interest LIKE ?) GROUP BY name, staff_id ORDER BY name ASC ";
+    " WHERE (staff_id LIKE ? OR name LIKE ?) OR staff_id IN (SELECT staff_id FROM ". $TABLES['research_interest'] ." WHERE interest LIKE ?) GROUP BY name, staff_id ORDER BY name ASC ";
+
 
 $query_rsStaff 			= "SELECT * FROM " . $TABLES['staff'];
 
@@ -123,15 +129,19 @@ try {
     asort($AL_Staff);
 
     $stmt_1 = $conn_db_ntu->prepare($query_rsResearchInterest);
-    $stmt_1->bindParam(1, $filter_StaffID);
+  //  $stmt_1->bindParam(1, $filter_StaffID);
+    $stmt_1->bindParam(1, $filter_Search);
     $stmt_1->bindParam(2, $filter_Search);
     $stmt_1->bindParam(3, $filter_Search);
-    $stmt_1->bindParam(4, $filter_Search);
+
     $stmt_1->execute();
     $rsResearchInterest = $stmt_1->fetchAll(PDO::FETCH_ASSOC);
+    print_r($rsResearchInterest);
     $AL_Staff_Filter 		= array();
     foreach ($rsResearchInterest as $key => $value) {
         $AL_Staff_Filter[$value["staff_id"]] = $value;
+
+
     }
     asort($AL_Staff_Filter);
     $Total_RowCount 	= count($AL_Staff_Filter);
@@ -150,35 +160,35 @@ $total_pages = ceil($Total_RowCount/$maxRow_researchInterest) - 1;
 //limit record to 20 per page
 $query_limit_rsResearchInterest = sprintf("%s LIMIT %d,%d", $query_rsResearchInterest, $startRow_researchInterest, $maxRow_researchInterest);
 
-try {
-    $stmt_0 			= $conn_db_ntu->prepare($query_rsStaff);
-    $stmt_0->execute();
-    $DBData_rsStaff 	= $stmt_0->fetchAll(PDO::FETCH_ASSOC);
-    $AL_Staff			= array();
-    foreach ($DBData_rsStaff as $key => $value) {
-        $AL_Staff[$value["id"]] = $value["name"];
-    }
-    asort($AL_Staff);
-
-    $stmt_1 = $conn_db_ntu->prepare($query_limit_rsResearchInterest);
-    $stmt_1->bindParam(1, $filter_StaffID);
-    $stmt_1->bindParam(2, $filter_Search);
-    $stmt_1->bindParam(3, $filter_Search);
-    $stmt_1->bindParam(4, $filter_Search);
-    $stmt_1->execute();
-    $rsResearchInterest = $stmt_1->fetchAll(PDO::FETCH_ASSOC);
-    $AL_Staff_Filter 		= array();
-    foreach ($rsResearchInterest as $key => $value) {
-        $AL_Staff_Filter[$value["staff_id"]] = $value;
-    }
-    asort($AL_Staff_Filter);
-    $Total_RowCount 	= count($AL_Staff_Filter);
-
-
-
-}catch (PDOException $e) {
-    die($e->getMessage());
-}
+// try {
+//     $stmt_0 			= $conn_db_ntu->prepare($query_rsStaff);
+//     $stmt_0->execute();
+//     $DBData_rsStaff 	= $stmt_0->fetchAll(PDO::FETCH_ASSOC);
+//     $AL_Staff			= array();
+//     foreach ($DBData_rsStaff as $key => $value) {
+//         $AL_Staff[$value["id"]] = $value["name"];
+//     }
+//     asort($AL_Staff);
+//
+//     $stmt_1 = $conn_db_ntu->prepare($query_rsResearchInterest);
+//     $stmt_1->bindParam(1, $filter_StaffID);
+//     $stmt_1->bindParam(2, $filter_Search);
+//     $stmt_1->bindParam(3, $filter_Search);
+//     $stmt_1->bindParam(4, $filter_Search);
+//     $stmt_1->execute();
+//     $rsResearchInterest = $stmt_1->fetchAll(PDO::FETCH_ASSOC);
+//     $AL_Staff_Filter 		= array();
+//     foreach ($rsResearchInterest as $key => $value) {
+//         $AL_Staff_Filter[$value["staff_id"]] = $value;
+//     }
+//     asort($AL_Staff_Filter);
+//     $Total_RowCount 	= count($AL_Staff_Filter);
+//
+//
+//
+// }catch (PDOException $e) {
+//     die($e->getMessage());
+// }
 
 $currentPage = $_SERVER ["PHP_SELF"];
 
@@ -358,6 +368,8 @@ $queryString_rsStaff = sprintf("&totalRows=%d%s", $Total_RowCount, $queryString_
                         echo "<td>" . $row_rsResearchInterest['staff_id'] . "</td>";
 
                         $interest = $row_rsResearchInterest["interests"];
+
+                        echo $interest;
                         if ($interest != "") {
                             $interest = str_replace('---', "\n", $interest);
                         }
